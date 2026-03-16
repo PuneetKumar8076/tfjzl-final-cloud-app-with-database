ce8da6b06d129e30dfb831feae8869b033246c4c
PUNEET KUMAR <kumarmahesh3594@gmail.com>
2026-03-16 23:50:41 +0530

    Update views.py

--- views.py
+++ views.py
@@ -1,35 +1,41 @@
-from django.shortcuts import render
-from .models import Question, Choice, Submission
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Course, Question, Choice, Submission
 
-def submit(request):
 
-```
-if request.method == "POST":
-    selected_choices = request.POST.getlist('choice')
+def submit(request, course_id):
+    course = get_object_or_404(Course, pk=course_id)
 
-    for choice_id in selected_choices:
-        choice = Choice.objects.get(id=choice_id)
-        Submission.objects.create(choice=choice)
+    if request.method == "POST":
+        selected_choices = request.POST.getlist('choice')
 
-return render(request, 'result.html')
-```
+        submission = Submission.objects.create()
 
-def show_exam_result(request):
+        for choice_id in selected_choices:
+            choice = Choice.objects.get(pk=choice_id)
+            submission.choices.add(choice)
 
-```
-submissions = Submission.objects.all()
+        return redirect('onlinecourse:show_exam_result',
+                        course_id=course.id,
+                        submission_id=submission.id)
 
-score = 0
-total = submissions.count()
 
-for submission in submissions:
-    if submission.choice.is_correct:
-        score += 1
+def show_exam_result(request, course_id, submission_id):
 
-context = {
-    'score': score,
-    'total': total
-}
+    submission = get_object_or_404(Submission, pk=submission_id)
 
-return render(request, 'exam_result.html', context)
-```
+    choices = submission.choices.all()
+
+    score = 0
+    total = choices.count()
+
+    for choice in choices:
+        if choice.is_correct:
+            score += 1
+
+    context = {
+        'score': score,
+        'total': total,
+        'course': course_id
+    }
+
+    return render(request, 'exam_result_bootstrap.html', context)
